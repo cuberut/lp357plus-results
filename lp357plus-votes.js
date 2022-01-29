a1d85e5ba0f7c15bc95ff79573f40ab2c4ab0986
cuberut <cuberut@gmail.com>
2022-01-30 00:16:48 +0100

    0.1 - init

---
+++ lp357plus-votes.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name       LP357+ votes
+// @version    0.1
+// @author     cuberut
+// @include    https://lista.radio357.pl/app/lista/wyniki/*
+// @updateURL  https://raw.githubusercontent.com/cuberut/lp357plus/main/lp357plus.js
+// @grant      GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("div#votes { position: absolute; left: 0; width: auto; text-align: center; }");
+
+const getTagVotes = (item) => `<div id="votes"><i class="${item.last ? 'fas' : 'far'} fa-star"></i><div class="small">(${item.count})</div></div>`;
+
+const chartNo = document.location.pathname.split("/").pop();
+
+const myVotes = {};
+
+for (var i = 1; i < chartNo+1; i++) {
+    const votes = JSON.parse(localStorage.getItem("myVotes" + i));
+    const last = (i == chartNo);
+
+    if (votes) {
+        votes.forEach(id => {
+            if (myVotes[id]) {
+                myVotes[id].count++;
+            } else {
+                myVotes[id] = { count: 1 };
+            }
+            if (last) {
+                myVotes[id].last = true;
+            }
+        });
+    }
+}
+
+const mainChart = [];
+const waitingRoom = [];
+
+XMLHttpRequest.prototype.realSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function(value) {
+    this.addEventListener("load", function(){
+        const textResponse = this.responseText;
+        const jsonResponse = JSON.parse(textResponse);
+        const dataResponse = jsonResponse.data.chart.results;
+        mainChart.push(...dataResponse.mainChart);
+        waitingRoom.push(...dataResponse.waitingRoom);
+    }, false);
+    this.realSend(value);
+};
+
+var ajaxRequest = new XMLHttpRequest();
+ajaxRequest.open("GET", "https://api.lista.radio357.pl/api/charts/lista/" + chartNo, false);
+ajaxRequest.send(null);
+
+let setList = [];
+
+if (mainChart.length) {
+    setList = mainChart.map((item, i) => ({id: item.id, votes: myVotes[item.id]}));
+}
+
+(function() {
+    let voteList;
+    const interval = setInterval(() => {
+        voteList = document.querySelector('.chart-list');
+        if (voteList) {
+            const items = [...voteList.querySelectorAll('.list-group-item')];
+            items.forEach((item, i) => {
+                if (setList[i].votes) {
+                    item.querySelector('.chart-item').insertAdjacentHTML('beforeend', getTagVotes(setList[i].votes));
+                }
+            });
+            clearInterval(interval);
+        }
+    }, 0);
+})();
